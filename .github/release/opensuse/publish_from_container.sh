c1fc1d01ebcac3fe6fd2a68fb927142f480601fc
matteo-chesi <matteo-chesi@users.noreply.github.com>
2025-11-25 15:33:55 +0100

    VCUE-1173 - add build on aarch64 and publish internally (#4)

---
+++ .github/release/opensuse/publish_from_container.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+VERSION_TAG="$1"
+cd "$(dirname $0)/../../../"
+DIST_DIR=".github/release/dist/"
+
+cd /tmp/
+#curl -sOL https://gitlab.com/gitlab-org/cli/-/releases/v1.78.2/downloads/glab_1.78.2_linux_amd64.rpm
+#zypper install --allow-unsigned-rpm -y bind-utils openssh ./glab_1.78.2_linux_amd64.rpm
+
+echo "ENVIRONMENT:"
+env
+echo "OS"
+cat /etc/os-release | grep PRETTY
+
+echo "GATHER SSH KNOWN HOSTS for git.cscs.ch"
+mkdir -p ~/.ssh
+ssh-keyscan git.cscs.ch >> ~/.ssh/known_hosts
+ssh-keyscan $(host git.cscs.ch | tail -n 1 | awk '{print $NF}') >> ~/.ssh/known_hosts
+echo "GIT CLONE"
+git clone git@git.cscs.ch:chesim/internal-skybox.git
+cd internal-skybox
+echo "TRIGGER GITLAB PIPELINE"
+glab ci run -b main --variables-env CI_DELIVER_JFROG:true --variables-env CI_RELEASE_TO_PUBLISH:${VERSION_TAG}
---
+++ .github/release/publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+VERSION_TAG="$1"
+THIS_DIR=$(readlink -f $(dirname $0))
+BASE_DIR=$(readlink -f "${THIS_DIR}/../../")
+DIST_DIR="${THIS_DIR}/dist"
+PRODUCT=$(basename ${BASE_DIR})
+cd $THIS_DIR
+
+. lib/common.sh
+check_build_os || exit 1
+check_slurm_version || exit 1
+check_build_container_image || exit 1
+
+# PUBLISH
+podman run --rm -ti \
+    -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
+    -v ${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK} \
+    -v ~/.config/glab-cli:/root/.config/glab-cli \
+    -v ${BASE_DIR}:/mnt \
+    ${BUILD_IMAGE_NAME} \
+    /mnt/.github/release/${BUILD_OS_NAME}/publish_from_container.sh ${VERSION_TAG}
