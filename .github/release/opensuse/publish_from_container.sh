41d26ccc4a7043a66b5b41854f8f0c3757cf9aeb
Matteo Chesi <chesi@cscs.ch>
2025-11-25 12:03:02 +0100

    Testing publishing workflow - 9

---
+++ .github/release/opensuse/publish_from_container.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+VERSION_TAG="$1"
+cd "$(dirname $0)/../../../"
+DIST_DIR=".github/release/dist/"
+
+cd /tmp/
+curl -sOL https://gitlab.com/gitlab-org/cli/-/releases/v1.78.2/downloads/glab_1.78.2_linux_amd64.rpm
+zypper install --allow-unsigned-rpm -y openssh ./glab_1.78.2_linux_amd64.rpm
+
+echo "ENVIRONMENT:"
+env
+echo "OS"
+cat /etc/os-release | grep PRETTY
+
+echo "GIT CLONE"
+git clone git@git.cscs.ch:chesim/internal-skybox.git
+cd internal-skybox
+glab ci run -b main --variables-env CI_DELIVER_JFROG:true --variables-env CI_RELEASE_TO_PUBLISH:${VERSION_TAG}
