ae905a0b4882783fca34f2df500ebaaf68ac6595
Matteo Chesi <chesi@cscs.ch>
2025-11-25 14:15:03 +0100

    Attempt - 1

--- .github/release/opensuse/publish_from_container.sh
+++ .github/release/opensuse/publish_from_container.sh
@@ -13,7 +13,11 @@ env
 echo "OS"
 cat /etc/os-release | grep PRETTY
 
+echo "GATHER SSH KNOWN HOSTS"
+mkdir -p ~/.ssh
+ssh-keyscan git.cscs.ch >> ~/.ssh/known_hosts
 echo "GIT CLONE"
 git clone git@git.cscs.ch:chesim/internal-skybox.git
 cd internal-skybox
+echo "TRIGGER GITLAB PIPELINE"
 glab ci run -b main --variables-env CI_DELIVER_JFROG:true --variables-env CI_RELEASE_TO_PUBLISH:${VERSION_TAG}
